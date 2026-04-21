86577641998aa803336d9282970e09fe8f6254a8
User06 <user06@vdesktop.sbcnoc.com>
2026-04-21 19:07:17 +0600

    added footer

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,5 @@
+export default function contact () {
+    return(
+            <div> This is contact </div>
+    );
+}
--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer className='bg-gray'>
+      <div className=''>
+        <p className="text-sm">
+          © {new Date().getFullYear()} My App. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
